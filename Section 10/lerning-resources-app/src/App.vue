b2f609c55e53bcab6fa34ddbc19bcc4751d9ac53
tiagoolifel <131689968+tiagoolifel@users.noreply.github.com>
2023-10-30 11:03:58 +0000

    Section 10 and Section 11

---
+++ Section 10/lerning-resources-app/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue'
+
+export default {
+  components: {
+    LearningResource
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'official guide',
+          description: 'the official vue.js',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'official-guide2',
+          title: 'official guide2',
+          description: 'the official vue.js',
+          link: 'https://vuejs.org'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ Section 10/lerning-resources-app/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></LearningResource>
+  </ul>
+</template>
+
+<script>
+
+export default{
+  props: ['storedResources']
+}
+</script>
